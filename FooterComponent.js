63a21ce21ce5330e197374cad5e4680661532e92
gudavilliradha <93853516+gudavilliradha@users.noreply.github.com>
2025-01-03 20:06:37 +0530

    Add files via upload

---
+++ FooterComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function FooterComponent() {
+  return (
+        <footer className='footer'>
+            <small>All Right Reserved &copy; RADHA</small>
+        </footer>
+  )
+}
+
+export default FooterComponent
